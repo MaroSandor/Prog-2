8d6c9d632bc360f28bfcf44573cff8996f538176
Sandor Maro <marosandor2001@gmail.com>
2024-09-16 21:49:28 +0200

    Updated bus-schedule

--- bus-schedule/src/Main.java
+++ bus-schedule/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random rand = new Random();
+
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = rand.nextInt(1000);
+        }
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
     }
 }
