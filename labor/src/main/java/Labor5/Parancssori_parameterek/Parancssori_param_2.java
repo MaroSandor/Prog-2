0031e2e5f0487b99e419355c48c0755c3848c421
Sandor Maro <marosandor2001@gmail.com>
2024-10-10 14:50:23 +0200

    param_2 StringBuilder

---
+++ labor/src/main/java/Labor5/Parancssori_parameterek/Parancssori_param_2.java
@@ -0,0 +1,19 @@
+package Labor5.Parancssori_parameterek;
+
+public class Parancssori_param_2 {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Adjono meg legalább 2 paramérert.");
+            System.exit(0);
+        }
+
+        // Az első argumentum a szó:
+        String word = args[0];
+        StringBuilder charSet = new StringBuilder();
+
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+
+    }
+}
