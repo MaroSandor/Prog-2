f2b09331818b12457d5ded2b31391be749be4456
Sandor Maro <marosandor2001@gmail.com>
2024-10-15 13:00:00 +0200

    commit

---
+++ zh/feladat1/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+class FootballTeam {
+    private List<String> players;
+
+    public FootballTeam() {
+        this.players = players;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FootballTeam realMadrid = new FootballTeam();
+    }
+}
